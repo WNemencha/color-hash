b4814eb95ea71e50ea011b439d83b3198af23360
Zeno Zeng <zenoofzeng@gmail.com>
2018-03-13 17:32:34 +0800

    [WIP] color-hash.ts

--- src/color-hash.ts
+++ src/color-hash.ts
@@ -1,72 +1,67 @@
-var BKDRHash = require('./bkdr-hash');
+import BKDRHash from './bkdr-hash'
 
-/**
- * Convert RGB Array to HEX
- *
- * @param {Array} RGBArray - [R, G, B]
- * @returns {String} 6 digits hex starting with #
- */
-var RGB2HEX = function(RGBArray) {
-    var hex = '#';
-    RGBArray.forEach(function(value) {
-        if (value < 16) {
-            hex += 0;
-        }
-        hex += value.toString(16);
-    });
-    return hex;
-};
+export default class ColorHash {
 
-/**
- * Convert HSL to RGB
- *
- * @see {@link http://zh.wikipedia.org/wiki/HSL和HSV色彩空间} for further information.
- * @param {Number} H Hue ∈ [0, 360)
- * @param {Number} S Saturation ∈ [0, 1]
- * @param {Number} L Lightness ∈ [0, 1]
- * @returns {Array} R, G, B ∈ [0, 255]
- */
-var HSL2RGB = function(H, S, L) {
-    H /= 360;
+    /**
+     * Convert RGB to HEX
+     * 
+     * @param r R ∈ [0, 255]
+     * @param g G ∈ [0, 255]
+     * @param b B ∈ [0, 255]
+     */
+    static rgb2hex(r, g, b: number) {
+        return [r, g, b].reduce((prev, curr) => prev + (curr < 16 ? `0${curr}` : curr), '#')
+    }
 
-    var q = L < 0.5 ? L * (1 + S) : L + S - L * S;
-    var p = 2 * L - q;
+    /**
+     * Convert HSL to RGB
+     * 
+     * @see {@link http://zh.wikipedia.org/wiki/HSL和HSV色彩空间} for further information.
+     * @param h Hue ∈ [0, 360)
+     * @param s Saturation ∈ [0, 1]
+     * @param l Lightness ∈ [0, 1]
+     * @returns {Array} R, G, B ∈ [0, 255]
+     */
+    static hsl2rgb(h, s, l: number) {
+        h /= 360;
 
-    return [H + 1/3, H, H - 1/3].map(function(color) {
-        if(color < 0) {
-            color++;
-        }
-        if(color > 1) {
-            color--;
-        }
-        if(color < 1/6) {
-            color = p + (q - p) * 6 * color;
-        } else if(color < 0.5) {
-            color = q;
-        } else if(color < 2/3) {
-            color = p + (q - p) * 6 * (2/3 - color);
-        } else {
-            color = p;
-        }
-        return Math.round(color * 255);
-    });
-};
+        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+        var p = 2 * l - q;
+    
+        return [h + 1/3, h, h - 1/3].map((color) => {
+            if(color < 0) {
+                color++;
+            }
+            if(color > 1) {
+                color--;
+            }
+            if(color < 1/6) {
+                color = p + (q - p) * 6 * color;
+            } else if(color < 0.5) {
+                color = q;
+            } else if(color < 2/3) {
+                color = p + (q - p) * 6 * (2/3 - color);
+            } else {
+                color = p;
+            }
+            return Math.round(color * 255);
+        });
+    }
 
-function isArray(o) {
-    return Object.prototype.toString.call(o) === '[object Array]';
 }
+
 
 /**
  * Color Hash Class
  *
  * @class
  */
-var ColorHash = function(options) {
+var ColorHashOld = function(options) {
     options = options || {};
 
     var LS = [options.lightness, options.saturation].map(function(param) {
         param = param || [0.35, 0.5, 0.65]; // note that 3 is a prime
-        return isArray(param) ? param.concat() : [param];
+        return Array.isArray(param) ? param.concat() : [param];
     });
 
     this.L = LS[0];
@@ -75,7 +70,7 @@ var ColorHash = function(options) {
     if (typeof options.hue === 'number') {
         options.hue = {min: options.hue, max: options.hue};
     }
-    if (typeof options.hue === 'object' && !isArray(options.hue)) {
+    if (typeof options.hue === 'object' && !Array.isArray(options.hue)) {
         options.hue = [options.hue];
     }
     if (typeof options.hue === 'undefined') {
@@ -109,9 +104,9 @@ ColorHash.prototype.hsl = function(str) {
     } else {
         H = hash % 359; // note that 359 is a prime
     }
-    hash = parseInt(hash / 360);
+    hash = Math.floor(hash / 360);
     S = this.S[hash % this.S.length];
-    hash = parseInt(hash / this.S.length);
+    hash = Math.floor(hash / this.S.length);
     L = this.L[hash % this.L.length];
 
     return [H, S, L];
@@ -140,4 +135,3 @@ ColorHash.prototype.hex = function(str) {
     return RGB2HEX(rgb);
 };
 
-module.exports = ColorHash;
